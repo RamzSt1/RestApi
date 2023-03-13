72b34c8ec97d2355210535690154709203d66529
Vi[Told] <Ira455436@gmail.com>
2023-03-13 22:57:23 +0900

    admin models

--- restapi/delivery_customer/admin.py
+++ restapi/delivery_customer/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from delivery_customer.models import *
 # Register your models here.
+
+@admin.register(Car)
+class AdminCar(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = list_display
+    search_fields = list_display
+    
+@admin.register(Gabarits)
+class AdminGabarits(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = list_display
+    search_fields = list_display
+    
+@admin.register(Status)
+class AdminStatus(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = list_display
+    search_fields = list_display
+
+@admin.register(Orders)
+class AdminOrders(admin.ModelAdmin):
+    list_display = ['customer', 'client', 'date', 'fio', 'address_a', 'address_b', 'gabarits', 'weigth', 'status', 'car']
+    list_display_links = list_display
+    search_fields = list_display
--- restapi/galacticexpress/admin.py
+++ restapi/galacticexpress/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from galacticexpress.models import *
+
+@admin.register(News)
+class AdminCar(admin.ModelAdmin):
+    list_display = ['title', 'text']
+    list_display_links = list_display
+    search_fields = list_display
